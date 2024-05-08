6671ce4a5fd314a16211979f928742cb204806fb
MaxRek <max.rekar@gmail.com>
2024-05-08 19:02:31 +0200

    debut d'instance

--- src/entity/instance/data.py
+++ src/entity/instance/data.py
@@ -1,1 +1,35 @@
+import numpy as np
+
 class Data:
+    N:int
+    C:int
+    P:int
+    K:int
+    F:int
+    Fs:int
+    Fp:int
+    Q:int
+    O:list
+    d:np.matrix
+    S:np.matrix
+    c:np.matrix
+    t:np.matrix
+
+    def __init__(self, n:int,c:int,p:int,k:int,f:int,fp:int,fs:int,q:int,o:list,d:np.matrix,S:np.matrix,ct:np.matrix,t:np.matrix) -> None:
+        if len(o) != n and len(d) != c and len(d[0]) != f and len(S) != p and len(t) != n and len(t[0]) != fs and fp < fs :
+            self.N = n
+            self.C = c
+            self.P = p
+            self.K = k
+            self.F = f
+            self.Fs = fs
+            self.Fp = fp
+            self.Q = q
+            self.O = o
+            self.d = d
+            self.S = S
+            self.c = ct
+            self.t = t
+        else:
+            print("Erreur d'initialisation, problème impossible")
+        
--- src/entity/instance/instance.py
+++ src/entity/instance/instance.py
@@ -1,12 +1,18 @@
 from ..df import Df
 from .decision import Dec
 from .data import Data
+import numpy as np
+import 
+
 
 class Instance:
     df:Df
     dec:Dec
     data:Data
 
-    def __init__(df) -> None:
-        pass
+    def __init__(self, N:list ,df:Df, K : int, F:int, Fs:int,Q : int, O:list, d:np.matrix, S:np.matrix, t:np.matrix) -> None:
+        self.df=df
+        C = df.E.shape[0]
+        P = df.F.shape[0]
+        self.dec = Dec()
 
