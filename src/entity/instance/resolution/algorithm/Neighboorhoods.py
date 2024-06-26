a6af344d0c30a5504b3f2210143ffc9b046cf8a6
MaxRK <77739780+MaxRek@users.noreply.github.com>
2024-06-26 14:08:43 +0200

    Revert "1.1"

--- src/entity/instance/resolution/algorithm/Neighboorhoods.py
+++ src/entity/instance/resolution/algorithm/Neighboorhoods.py
@@ -7,8 +7,6 @@ import copy
 import time
 from ..tools import rand_ind_in_list, reduct_LnfPT, calc_LnfPT_c, get_fs_prod_ind_qt, get_sum_qt_c_l_by_d
 from .Neighboorhoods_gen import *
-from .CAW import CAW_F
-
 
 
 #INSERTION intra_tournee
@@ -19,9 +17,9 @@ from .CAW import CAW_F
 #   index_tournee : 0 à nb de tournee de tel type
 #   indexes_affect : (index_sommet à insérer, index_insertion du sommet)
 # ]
-def N1_intra(x:Solution, data:Sub_data, entry:list = [-2,-2,-2,[-2,-2]]):
+def N1_intra(x:Solution, data:Sub_data, entry:list = [-1,-1,-1,[-1,-1]]):
     #si index_plat non déterminé, assignation à une plat ouverte ou transf    
-    if entry[0] == -2 or entry[1] == -2 or entry[2] == -2 or entry[3][0] == -2:
+    if entry[0] == -1 or entry[1] == -1 or entry[2] == -1 or entry[3][0] == -1:
         e = N1_intra_rand(x,entry)
     else:
         e = copy.deepcopy(entry)
@@ -30,7 +28,7 @@ def N1_intra(x:Solution, data:Sub_data, entry:list = [-2,-2,-2,[-2,-2]]):
 
     xp = copy.deepcopy(x)
     #Dans le cadre de la collecte sale
-    if e[0] == -1:
+    if e[0] == -2:
         t0 = xp.sales[e[2]]
         t0.reinsert(e[3][0],e[3][1])
         # x.sales[e[2]].print_tournee()
@@ -51,21 +49,21 @@ def N1_intra(x:Solution, data:Sub_data, entry:list = [-2,-2,-2,[-2,-2]]):
 #   indexes_tournee : (0 à nb de tournee de tel type, 0 à nb de tournee de tel type)
 #   index_affect : (index_sommet à ré-insérer, index où réinsérer sommet)
 # ]
-def N1_inter(x:Solution, data:Sub_data, entry = [-2,-2,[-2,-2],[-2,-2]]):
-    if entry[0] == -2 or entry[1] == -2 or entry[2][0] == -2 or entry[2][1] == -2 or entry[3] == -2:
+def N1_inter(x:Solution, data:Sub_data, entry = [-1,-1,[-1,-1],[-1,-1]]):
+    if entry[0] == -1 or entry[1] == -1 or entry[2][0] == -1 or entry[2][1] == -1 or entry[3] == -1:
         e = N1_inter_rand(x,entry)
     else:
         e = copy.deepcopy(entry)
-    # print(entry)     
-    # print(e)
+    print(entry)     
+    print(e)
 
     xp = copy.deepcopy(x)
-    if e[0] == -1:
+    if e[0] == -2:
 
         t0 = xp.sales[e[2][0]]
         t1 = xp.sales[e[2][1]]
-        # x.sales[e[2][0]].print_tournee()
-        # x.sales[e[2][1]].print_tournee()
+        x.sales[e[2][0]].print_tournee()
+        x.sales[e[2][1]].print_tournee()
 
         s = t0.pop(e[3][0])
         t1.insert_sommet(s,e[3][1])
@@ -75,21 +73,25 @@ def N1_inter(x:Solution, data:Sub_data, entry = [-2,-2,[-2,-2],[-2,-2]]):
             xp.sales.pop(e[2][0])
         # print("La tournée a été supprimée : "+str(deleted))
         
-        fs= get_fs_prod_ind_qt(data.rev_d)
-
         if not deleted:
-            t0.calc_load(fs)
-            # t0.print_tournee()
-        t1.calc_load(fs)
+            if e[1] == 0: #Tournée de collecte
+                t0.calc_load(reduct_LnfPT(xp.plat[e[0]].Lfptn,data.C))
+            else: #Tournée de livraison
+                t0.load = sum(get_sum_qt_c_l_by_d(data.d, t0.order,data.F))
+            t0.print_tournee()
+        if e[1] == 0: #Tournée de collecte
+            t1.calc_load(reduct_LnfPT(xp.plat[e[0]].Lfptn,data.C))
+        else: #Tournée de livraison
+            t1.load = sum(get_sum_qt_c_l_by_d(data.d, t1.order,data.F))
         # t1.print_tournee()
         if(t0.load > data.Q or t1.load > data.Q):
-            #print("Insertion impossible pour points "+str(e[3])+" de tournée "+str(e[2][0])+" à "+str(e[2][1]))
+            print("Insertion impossible pour points "+str(e[3])+" de tournée "+str(e[2][0])+" à "+str(e[2][1]))
             xp = x
     else:
         t0 = xp.plat[e[0]].tournees[e[1]][e[2][0]]
         t1 = xp.plat[e[0]].tournees[e[1]][e[2][1]]
-        # x.plat[e[0]].tournees[e[1]][e[2][0]].print_tournee()
-        # x.plat[e[0]].tournees[e[1]][e[2][1]].print_tournee()
+        x.plat[e[0]].tournees[e[1]][e[2][0]].print_tournee()
+        x.plat[e[0]].tournees[e[1]][e[2][1]].print_tournee()
         
         # t0.print_tournee()
         # t1.print_tournee()
@@ -98,7 +100,7 @@ def N1_inter(x:Solution, data:Sub_data, entry = [-2,-2,[-2,-2],[-2,-2]]):
 
         #Verifiez si la tounrée 0 n'est pas vide apres avoir pris un sommet
         deleted = xp.plat[e[0]].tournee_post_del_point(e[1],e[2][0])
-        # print("La tournée a été supprimée : "+str(deleted))
+        print("La tournée a été supprimée : "+str(deleted))
         
         if not deleted:
             if e[1] == 0: #Tournée de collecte
@@ -114,7 +116,7 @@ def N1_inter(x:Solution, data:Sub_data, entry = [-2,-2,[-2,-2],[-2,-2]]):
         
         
         if t1.load > data.Q:
-            # print("Insertion impossible pour points "+str(e[3])+" de tournée "+str(e[2][0])+" à "+str(e[2][1]))
+            print("Insertion impossible pour points "+str(e[3])+" de tournée "+str(e[2][0])+" à "+str(e[2][1]))
             xp = x
 
     return xp
@@ -128,8 +130,8 @@ def N1_inter(x:Solution, data:Sub_data, entry = [-2,-2,[-2,-2],[-2,-2]]):
 #   indexes_affect : (index_sommet à swap, index_sommet à swap)
 # ]
 
-def N2_intra(x:Solution, data:Sub_data, entry = [-2,-2,-2,[-2,-2]]):
-    if entry[0] == -2 or entry[1] == -2 or entry[2] == -2 or entry[3][0] == -2:
+def N2_intra(x:Solution, data:Sub_data, entry = [-1,-1,-1,[-1,-1]]):
+    if entry[0] == -1 or entry[1] == -1 or entry[2] == -1 or entry[3][0] == -1:
         e = N2_intra_rand(x,entry)
     else:
         e = copy.deepcopy(entry)
@@ -137,7 +139,7 @@ def N2_intra(x:Solution, data:Sub_data, entry = [-2,-2,-2,[-2,-2]]):
     # print(e)
     xp = copy.deepcopy(x)
 
-    if e[0] == -1:
+    if e[0] == -2:
         t0 = xp.sales[e[2]]
         t0.swap_two_s_order(e[3][0],e[3][1])
         # x.sales[e[2]].print_tournee()
@@ -158,8 +160,8 @@ def N2_intra(x:Solution, data:Sub_data, entry = [-2,-2,-2,[-2,-2]]):
 #   index_tournee : (0 à nb de tournee de tel type,0 à nb de tournee de tel type)
 #   indexes_affect : (index_sommet à swap dans tournée 1, index_sommet à swap dans tournée 2)
 # ]
-def N2_inter(x:Solution, data:Sub_data, entry = [-2,-2,[-2,-2],[-2,-2]]):
-    if entry[0] == -2 or entry[1] == -2 or entry[2][0] == -2 or entry[3][0] == -2:
+def N2_inter(x:Solution, data:Sub_data, entry = [-1,-1,[-1,-1],[-1,-1]]):
+    if entry[0] == -1 or entry[1] == -1 or entry[2][0] == -1 or entry[3][0] == -1:
         e = N2_inter_rand(x,entry)
     else:
         e = copy.deepcopy(entry)
@@ -168,7 +170,7 @@ def N2_inter(x:Solution, data:Sub_data, entry = [-2,-2,[-2,-2],[-2,-2]]):
     
     xp = copy.deepcopy(x)
     
-    if e[0] == -1:
+    if e[0] == -2:
         t0 = xp.sales[e[2][0]]
         t1 = xp.sales[e[2][1]]
         # t0.print_tournee()
@@ -221,26 +223,26 @@ def N2_inter(x:Solution, data:Sub_data, entry = [-2,-2,[-2,-2],[-2,-2]]):
 #   index_tournee : 0 à nb de tournee de tel type
 #   indexes_affect : ([index_debut seq, index_fin seq], index où inserer seq)
 # ]
-def N3_intra(x:Solution, data:Sub_data, entry = [-2,-2,-2,[[-2,-2],-2]]):
-    if entry[0] == -2 or entry[1] == -2 or entry[2] == -2 or entry[3][0][0] == -2 or entry[3][0] == -2 :
+def N3_intra(x:Solution, data:Sub_data, entry = [-1,-1,-1,[[-1,-1],-1]]):
+    if entry[0] == -1 or entry[1] == -1 or entry[2] == -1 or entry[3][0][0] == -1 or entry[3][0] == -1 :
         e = N3_intra_rand(x,entry)
     else:
         e = copy.deepcopy(entry)
-    # print(entry)     
-    # print(e)
+    print(entry)     
+    print(e)
 
     xp = copy.deepcopy(x)
-    if e[0] == -1:
+    if e[0] == -2:
         t0 = xp.sales[e[2]]
 
         t0.extended_or_opt(e[3][0], e[3][1])
-        # x.sales[e[2]].print_tournee()
-        # t0.print_tournee()
+        x.sales[e[2]].print_tournee()
+        t0.print_tournee()
     else:
         t0 = xp.plat[e[0]].tournees[e[1]][e[2]]
         t0.extended_or_opt(e[3][0], e[3][1])
-        # x.plat[e[0]].tournees[e[1]][e[2]].print_tournee()
-        # t0.print_tournee()
+        x.plat[e[0]].tournees[e[1]][e[2]].print_tournee()
+        t0.print_tournee()
     return xp
 
 #EXTENDED OR_OPT inter_tournee
@@ -251,20 +253,20 @@ def N3_intra(x:Solution, data:Sub_data, entry = [-2,-2,-2,[[-2,-2],-2]]):
 #   index_tournee : (0 à nb de tournee de tel type, 0 à nb de tournee de tel type)
 #   indexes_affect : ([index_debut seq, index_fin seq], index où inserer seq dans t2)
 # ]
-def N3_inter(x:Solution, data:Sub_data, entry = [-2,-2,[-2,-2],[[-2,-2],-2]]):
-    if entry[0] == -2 or entry[1] == -2 or entry[2][0] == -2 or entry[3][0][0] == -2 or entry[3][0] == -2 :
+def N3_inter(x:Solution, data:Sub_data, entry = [-1,-1,[-1,-1],[[-1,-1],-1]]):
+    if entry[0] == -1 or entry[1] == -1 or entry[2][0] == -1 or entry[3][0][0] == -1 or entry[3][0] == -1 :
         e = N3_inter_rand(x,entry)
     else:
         e = copy.deepcopy(entry)
-    # print(entry)     
-    # print(e)
+    print(entry)     
+    print(e)
     xp = copy.deepcopy(x)
     
-    if e[0] == -1:
+    if e[0] == -2:
         t0 = xp.sales[e[2][0]]
         t1 = xp.sales[e[2][1]]
-        # x.sales[e[2][0]].print_tournee()
-        # x.sales[e[2][1]].print_tournee()
+        x.sales[e[2][0]].print_tournee()
+        x.sales[e[2][1]].print_tournee()
 
         seq = t0.order[e[3][0][0]:e[3][0][1]]
         for i in range(e[3][0][1]-e[3][0][0]):
@@ -274,7 +276,7 @@ def N3_inter(x:Solution, data:Sub_data, entry = [-2,-2,[-2,-2],[[-2,-2],-2]]):
         deleted = xp.sales[e[2][0]].size == 0
         if deleted:
             xp.sales.pop(e[2][0])
-        # print("La tournée a été supprimée : "+str(deleted))
+        print("La tournée a été supprimée : "+str(deleted))
         
         fs= get_fs_prod_ind_qt(data.rev_d)
         t0.calc_load(fs)
@@ -282,18 +284,18 @@ def N3_inter(x:Solution, data:Sub_data, entry = [-2,-2,[-2,-2],[[-2,-2],-2]]):
 
         if not deleted:
             t0.calc_load(fs)
-            # t0.print_tournee()
+            t0.print_tournee()
         t1.calc_load(fs)
-        # t1.print_tournee()
+        t1.print_tournee()
 
         if(t1.load > data.Q):
-            # print("Insertion impossible pour points "+str(e[3])+" de tournée "+str(e[2][0])+" à "+str(e[2][1]))
+            print("Insertion impossible pour points "+str(e[3])+" de tournée "+str(e[2][0])+" à "+str(e[2][1]))
             xp = x
     else:
         t0 = xp.plat[e[0]].tournees[e[1]][e[2][0]]
         t1 = xp.plat[e[0]].tournees[e[1]][e[2][1]]
-        # x.plat[e[0]].tournees[e[1]][e[2][0]].print_tournee()
-        # x.plat[e[0]].tournees[e[1]][e[2][1]].print_tournee()
+        x.plat[e[0]].tournees[e[1]][e[2][0]].print_tournee()
+        x.plat[e[0]].tournees[e[1]][e[2][1]].print_tournee()
         
         seq = t0.order[e[3][0][0]:e[3][0][1]]
         for i in range(e[3][0][1]-e[3][0][0]):
@@ -302,7 +304,7 @@ def N3_inter(x:Solution, data:Sub_data, entry = [-2,-2,[-2,-2],[[-2,-2],-2]]):
 
         #Verifiez si la tounrée 0 n'est pas vide apres avoir pris un sommet
         deleted = xp.plat[e[0]].tournee_post_del_point(e[1],e[2][0])
-        # print("La tournée a été supprimée : "+str(deleted))
+        print("La tournée a été supprimée : "+str(deleted))
         
         if not deleted:
             if e[1] == 0: #Tournée de collecte
@@ -318,7 +320,7 @@ def N3_inter(x:Solution, data:Sub_data, entry = [-2,-2,[-2,-2],[[-2,-2],-2]]):
         
         
         if t1.load > data.Q:
-            # print("Insertion impossible pour points "+str(e[3])+" de tournée "+str(e[2][0])+" à "+str(e[2][1]))
+            print("Insertion impossible pour points "+str(e[3])+" de tournée "+str(e[2][0])+" à "+str(e[2][1]))
             xp = x
 
     return xp
@@ -332,8 +334,8 @@ def N3_inter(x:Solution, data:Sub_data, entry = [-2,-2,[-2,-2],[[-2,-2],-2]]):
 #   indexes_affect : ([index_debut seq, index_fin seq], index où inserer seq dans t2)
 # ]
 # (Utilise le même générateur que pour N3_intra parce que même structure)
-def N4_intra(x:Solution, data:Sub_data, entry = [-2,-2,-2,[[-2,-2],-2]]):
-    if entry[0] == -2 or entry[1] == -2 or entry[2] == -2 or entry[3][0][0] == -2 or entry[3][0] == -2 :
+def N4_intra(x:Solution, data:Sub_data, entry = [-1,-1,-1,[[-1,-1],-1]]):
+    if entry[0] == -1 or entry[1] == -1 or entry[2] == -1 or entry[3][0][0] == -1 or entry[3][0] == -1 :
         e = N3_intra_rand(x,entry)
     else:
         e = copy.deepcopy(entry)
@@ -341,7 +343,7 @@ def N4_intra(x:Solution, data:Sub_data, entry = [-2,-2,-2,[[-2,-2],-2]]):
     # print(e)
 
     xp = copy.deepcopy(x)
-    if e[0] == -1:
+    if e[0] == -2:
         t0 = xp.sales[e[2]]
         t0.inverse_or_opt(e[3][0], e[3][1])
         # x.sales[e[2]].print_tournee()
@@ -361,16 +363,16 @@ def N4_intra(x:Solution, data:Sub_data, entry = [-2,-2,-2,[[-2,-2],-2]]):
 #   index_tournee : (0 à nb de tournee de tel type, 0 à nb de tournee de tel type)
 #   indexes_affect : ([index_debut seq, index_fin seq], index où inserer seq dans t2)
 # ]
-def N4_inter(x:Solution, data:Sub_data, entry = [-2,-2,[-2,-2],[[-2,-2],-2]]):
-    if entry[0] == -2 or entry[1] == -2 or entry[2][0] == -2 or entry[3][0][0] == -2 or entry[3][0] == -2 :
+def N4_inter(x:Solution, data:Sub_data, entry = [-1,-1,[-1,-1],[[-1,-1],-1]]):
+    if entry[0] == -1 or entry[1] == -1 or entry[2][0] == -1 or entry[3][0][0] == -1 or entry[3][0] == -1 :
         e = N3_inter_rand(x,entry)
     else:
         e = copy.deepcopy(entry)
-    # print(entry)     
-    # print(e)
+    print(entry)     
+    print(e)
     xp = copy.deepcopy(x)
     
-    if e[0] == -1:
+    if e[0] == -2:
         t0 = xp.sales[e[2][0]]
         t1 = xp.sales[e[2][1]]
         # x.sales[e[2][0]].print_tournee()
@@ -412,7 +414,7 @@ def N4_inter(x:Solution, data:Sub_data, entry = [-2,-2,[-2,-2],[[-2,-2],-2]]):
 
         #Verifiez si la tounrée 0 n'est pas vide apres avoir pris un sommet
         deleted = xp.plat[e[0]].tournee_post_del_point(e[1],e[2][0])
-        # print("La tournée a été supprimée : "+str(deleted))
+        print("La tournée a été supprimée : "+str(deleted))
         
         if not deleted:
             if e[1] == 0: #Tournée de collecte
@@ -428,7 +430,7 @@ def N4_inter(x:Solution, data:Sub_data, entry = [-2,-2,[-2,-2],[[-2,-2],-2]]):
         
         
         if t1.load > data.Q:
-            # print("Insertion impossible pour points "+str(e[3])+" de tournée "+str(e[2][0])+" à "+str(e[2][1]))
+            print("Insertion impossible pour points "+str(e[3])+" de tournée "+str(e[2][0])+" à "+str(e[2][1]))
             xp = x
 
     return xp
@@ -438,8 +440,8 @@ def N4_inter(x:Solution, data:Sub_data, entry = [-2,-2,[-2,-2],[[-2,-2],-2]]):
 # [
 #   index_plat_a_ouvrir : index de plat ouverte,
 # ]
-def N5_Add(x:Solution, data:Sub_data, entry = [-2]):
-    if entry[0] == -2:
+def N5_Add(x:Solution, data:Sub_data, entry = [-1]):
+    if entry[0] == -1:
         e = N5_add_rand(x,data.N,entry)
     else:
         e = copy.deepcopy(entry)
@@ -453,8 +455,8 @@ def N5_Add(x:Solution, data:Sub_data, entry = [-2]):
 # [
 #   index_plat_a_fermer : emplacement dans x.plat à pop,
 # ]
-def N5_Del(x:Solution, data:Sub_data, entry = [-2]):
-    if entry[0] == -2:
+def N5_Del(x:Solution, data:Sub_data, entry = [-1]):
+    if entry[0] == -1:
         e = N5_del_rand(x,data.N,entry)
     else:
         e = copy.deepcopy(entry)
@@ -483,8 +485,8 @@ def N5_Del(x:Solution, data:Sub_data, entry = [-2]):
 #   index_plat_a_ouvrir : 0 à numéro de plat à ouvrir,
 # ]
 
-def N5_Swap(x:Solution, data:Sub_data, entry = [-2,-2]):
-    if entry[0] == -2 or entry[1] == -2:
+def N5_Swap(x:Solution, data:Sub_data, entry = [-1,-1]):
+    if entry[0] == -1 or entry[1] == -1:
         e = N5_swap_rand(x,data.N,entry)
     else:
         e = copy.deepcopy(entry)
@@ -499,8 +501,8 @@ def N5_Swap(x:Solution, data:Sub_data, entry = [-2,-2]):
 #   numero_client à réaffecter
 #   numero_plat où réaffecter client
 # ]
-def N6_one(x:Solution, data:Sub_data, entry = [-2,-2]):
-    if entry[0] == -2 or entry[1] == -2:
+def N6_one(x:Solution, data:Sub_data, entry = [-1,-1]):
+    if entry[0] == -1 or entry[1] == -1:
         e = N6_one_rand(x,data.C,data.N,entry)
     else:
         e = copy.deepcopy(entry)
@@ -536,23 +538,21 @@ def N6_one(x:Solution, data:Sub_data, entry = [-2,-2]):
 #   [liste de numero_client à réaffecter]
 #   [liste où réaffecter num_cli dans premier paramètre]
 # # ]
-def N6_some(x:Solution, data:Sub_data, entry = [[-2],[-2]]):
-    if entry[0] == [-2] or entry[1] == [-2]:
+def N6_some(x:Solution, data:Sub_data, entry = [[-1],[-1]]):
+    if entry[0] == [-1] or entry[1] == [-1]:
         e = N6_some_rand(x,data.C,data.N,entry)
-        print("gen")
-    elif len(entry[0][0]) == len(entry[0][1]) :
+    else:
         e = copy.deepcopy(entry)
     # print(entry)
     # print(e)
     
     xp = copy.deepcopy(x)
 
-    for s in range(2,len(entry[0])):
+    for s in range(len(entry[0])):
         found = False
         i = 0
-
+        
         while not found and i < len(x.plat):
-            print(i)
             if e[0][s] in xp.plat[i].cli_affect:
                 found = True
             else:
@@ -578,31 +578,8 @@ def N6_all(x:Solution, data:Sub_data):
         p.pt_affect.clear()
         p.tournees[0].clear()
         p.tournees[1].clear()
-        
-    
-    return xp
 
-def N6_reaffect(x:Solution, data:Sub_data):
-    xp = copy.deepcopy(x)
-    for p in xp.plat:
-        p.cli_affect.clear()
-        p.pt_affect.clear()
-        p.tournees[0].clear()
-        p.tournees[1].clear()
-
-    for c in range(data.N,data.C):
-        min_c = []
-        for p in xp.plat:
-            min_c.append(data.c[p.numero][c])
-        xp.plat[min_c.index(min(min_c))].add_client(data.d,c)
-
-    for p in xp.plat:
-        p.calc_LnfPT_c(data.d, data.C, data.F, data.T,data.T, data.Q)
-        
-    xp.init_CAW_cp(data.c, data.Q, data.C)
-    xp.init_CAW_lp(data.c, data.Q, data.D, data.F)
-    # xp.init_CAW_cp(data.C, data.Q, data.C)
-    # xp.init_CAW_lp(data.c, data.Q, data.d, data.F)
     xp.repair_solution_post_N6(data)
+
 
     return xp
